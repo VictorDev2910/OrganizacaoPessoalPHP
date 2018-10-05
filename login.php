7a0c9d1c28b53c3eaef72e04257dee211bc2b5ce
Victor <victordesenv@outlook.com>
2018-10-05 09:52:46 -0300

    Inicio do projeto

---
+++ login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Organização Pessoal</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="card">
+        <div class="card-block">
+            <div class="text-center">
+                <h3><i class="fa fa-lock"></i> Login:</h3>
+                <hr class="mt-2 mb-2">
+            </div>
+            <div class="md-form">
+                <i class="fa fa-envelope prefix"></i>
+                <input type="text" id="form2" class="form-control">
+                <label for="form2">Email</label>
+            </div>
+            <!-- Corpo do painel -->
+            <div class="md-form">
+                <i class="fa fa-lock prefix"></i>
+                <input type="password" id="form4" class="form-control">
+                <label for="form4">Senha</label>
+            </div>
+            <div class="text-center">
+                <a class="btn btn-primary btn-rounded">Login</a>
+            </div> 
+        </div>
+    </div>
+
+     <!--Rodapé-->
+     <div class="modal-footer">
+        <div class="options">
+            <p>Não é cadastrado? <a href="#">Cadastre-se</a></p>
+            <p>Esqueceu <a href="#">sua senha?</a></p>
+        </div>
+    </div>
+
+</div>
+    
+  
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+</body>
+
+</html>
